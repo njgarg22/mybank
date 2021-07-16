cfd2ca98ca6e8fcfa38b38f06bfa9aa2cd7527a2
Neeraj Garg <njgarg22@gmail.com>
2021-07-17 01:44:40 +0530

    Update ApplicationLauncher.java

--- src/main/java/com/neeraj/mybank/ApplicationLauncher.java
+++ src/main/java/com/neeraj/mybank/ApplicationLauncher.java
@@ -9,26 +9,26 @@ import com.neeraj.mybank.web.TransactionServlet;
 
 public class ApplicationLauncher {
 
-	public static void main(String[] args) throws LifecycleException {
-		/*
-		 *  Run `java -jar -Dserver.port=8090 {jarname}.jar' & Tomcat boots up on port 8090, instead of 8080.  
-		 *  Default to port 8080, if no system property is set.
-		 */
+    public static void main(String[] args) throws LifecycleException {
+        /*
+         *  Run `java -jar -Dserver.port=8090 {jarname}.jar' & Tomcat boots up on port 8090, instead of 8080.
+         *  Default to port 8080, if no system property is set.
+         */
         int serverPort = 8080;
         String portProperty = System.getProperty("server.port");
         if (portProperty != null) {
             serverPort = Integer.parseInt(portProperty);
         }
-        
-		Tomcat tomcat = new Tomcat();
-		tomcat.setPort(serverPort);
-		tomcat.getConnector();
-		
-		Context ctx = tomcat.addContext("", null);
-		Wrapper servletWrapper = Tomcat.addServlet(ctx, "TransactionServlet", new TransactionServlet());
-		servletWrapper.setLoadOnStartup(1);
-		servletWrapper.addMapping("/*");
-		
-		tomcat.start();
-	}
+
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(serverPort);
+        tomcat.getConnector();
+
+        Context ctx = tomcat.addContext("", null);
+        Wrapper servletWrapper = Tomcat.addServlet(ctx, "TransactionServlet", new TransactionServlet());
+        servletWrapper.setLoadOnStartup(1);
+        servletWrapper.addMapping("/*");
+
+        tomcat.start();
+    }
 }
